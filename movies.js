2e0275356ea9f53ecff8b113d08d0482a900025a
ramana kolli <58178560+17BQ1A1272@users.noreply.github.com>
2022-10-21 15:04:51 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -17,6 +17,7 @@ var updateCallback = function(data){
     }
     ).then(function(res){
     document.getElementById("Title").innerHTML = res.Title;
+    document.getElementById("Hero").innerHTML = res.Hero;
     document.getElementById("Year").innerHTML = res.Year;
     document.getElementById("Genre").innerHTML = res.Genre;
     document.getElementById("Plot").innerHTML = res.Plot;
